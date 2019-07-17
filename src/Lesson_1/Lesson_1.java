a114b5e34cc4bd6b3ec43e8fbcf2057f545ad433
Maxim Chistyakov <gods.like.you@gmail.com>
2019-07-17 12:18:02 +0300

    first deploy

---
+++ src/Lesson_1/Lesson_1.java
@@ -0,0 +1,15 @@
+package Lesson_1;
+
+public class Lesson_1 {
+
+    int a = 10;
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void test() {
+        int b = 10;
+        String c = "sadf";
+    }
+}
