bb6da29824d4a65cb4633edd51e188d8811848c4
mchist <maxim.chistyakov@devim.team>
2019-07-22 09:58:01 +0300

    Some shit with Team command

--- src/Lesson_1/Marathon/Team.java
+++ src/Lesson_1/Marathon/Team.java
@@ -2,7 +2,9 @@ package Lesson_1.Marathon;
 
 public class Team {
     private Competitor[] teamMembers;
-    public Team (String[][] ... name) {
+    private String commandName;
+
+    public Team (String[] ... name) {
         for (int i = 0; i < name.length; i++) {
             if (name[i][0].equals("Кот") ) {
                 teamMembers[i] = new Cat(name[i][1].toString() );
@@ -14,5 +16,12 @@ public class Team {
                 teamMembers[i] = new Human(name[i][1].toString());
             }
         }
+    }
+
+    public void setCommandName(String commandName) {
+        this.commandName = commandName;
+    }
+    public String getCommandName() {
+        return commandName;
     }
 }
