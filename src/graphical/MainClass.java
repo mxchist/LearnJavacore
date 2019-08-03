765db1308de492f26a52805eb29f6680a660e4e3
Maxim Chistyakov <gods.like.you@gmail.com>
2019-08-03 16:45:03 +0300

    grahical class

---
+++ src/graphical/MainClass.java
@@ -0,0 +1,7 @@
+package graphical;
+
+public class MainClass {
+    public static void main(String[] args) {
+        new MyWindow();
+    }
+}
---
+++ src/graphical/MyWindow.java
@@ -0,0 +1,58 @@
+package graphical;
+import javax.swing.*;
+import java.awt.GridLayout;
+import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyWindow extends JFrame {
+    public MyWindow() {
+        setBounds(500, 200, 800, 600);
+        setTitle("GUI Demo");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLayout(new GridLayout(2, 2));
+        JPanel[] jp = new JPanel[4];
+        for (int i = 0; i < 4; i++) {
+            jp[i] = new JPanel();
+            add(jp[i]);
+            jp[i].setBackground(new Color(100 + i * 40, 100 + i * 40, 100 + i * 40));
+        }
+
+        jp[0].setLayout(new BorderLayout());
+        JTextArea jta = new JTextArea();
+        JScrollPane jsp = new JScrollPane(jta);
+        jp[0].add(jsp);
+
+        setVisible(true);
+
+        jp[1].setLayout(new FlowLayout());
+        JRadioButton[] jrb = new JRadioButton[4];
+        ButtonGroup bgr = new ButtonGroup();
+        for (int i = 0; i < jrb.length; i++) {
+            jrb[i] = new JRadioButton("Option #" + i);
+            bgr.add(jrb[i]);
+            jp[1].add(jrb[i]);
+        }
+        JCheckBox[] jcb = new JCheckBox[4];
+        for (int i = 0; i < jcb.length; i++) {
+            jcb[i] = new JCheckBox("Check #" + i);
+            jp[1].add(jcb[i]);
+        }
+
+        jp[2].setLayout(new FlowLayout());
+        String[] comboStr = {"Item #1", "Item #2", "Item #3", "Item #4"};
+        JComboBox<String> jcombo1 = new JComboBox<String>(comboStr);
+        JComboBox<String> jcombo2 = new JComboBox<String>(comboStr);
+        jp[2].add(jcombo1);
+        jp[2].add(jcombo2);
+        jcombo1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println(jcombo1.getSelectedItem().toString());
+            }
+        });
+
+    }
+}
