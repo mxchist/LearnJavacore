25e14a7c76235cf9a219d17b7f1a6648bf010588
Maxim Chistyakov <gods.like.you@gmail.com>
2019-08-14 10:47:02 +0300

    multithreading - begin develop

--- src/multithreading/testClass.java
+++ src/multithreading/testClass.java
@@ -28,47 +28,33 @@ public class testClass {
         System.out.println("\n" + "Вычисление массива через потоки.");
         long a = System.currentTimeMillis();
 
-        float[] res1, res2, res;
+        float[] res1, res;
         res = new float[size];
 
-        int lbegin, lend = 0;
-        int threadQty = 6;
+        int lbegin;
+        int lend = -1;
+        int threadQty = 6;          //Количество потоков
 
-        for (int l =0; l < threadQty; l++) {
+        for (int l =1; l <= threadQty; l++) {
             long b = System.currentTimeMillis();
-            lbegin = lend;
-            lend = lbegin + size/threadQty - 1;
-            if (lend > arr.length) lend = arr.length - 1;
+            lbegin = lend +1;
+            lend = lbegin + size/threadQty;
+            if (lend >= arr.length) lend = arr.length - 1;
 
             float[] tempArray = new float[lend - lbegin];
             System.arraycopy(arr,lbegin,tempArray, 0, lend - lbegin);
             ThreadClass tempThread = new ThreadClass(tempArray);
             res1 = tempThread.getArr();
-            System.arraycopy(tempArray, lbegin, res, lend, lend - lbegin);
+            System.arraycopy(tempArray, lbegin, res, lend, lend - lbegin + 1);
 
             System.out.printf("Время цикла - %f милисекунд\n", (float) (System.currentTimeMillis() - b));
         }
-
-        float[] a1 = new float[size/2];
-        float[] a2 = new float[size/2];
-
-        System.arraycopy(arr, 0, a1, 0, h);
-        System.arraycopy(arr, h, a2, 0, h);
-
-        ThreadClass tc1 = new ThreadClass(a1);
-        ThreadClass tc2 = new ThreadClass(a2);
-
-        res1 = tc1.getArr();
-        res2 = tc2.getArr();
-
-        System.arraycopy(res1, 0, res, 0, h);
-        System.arraycopy(res1, 0, res, h, h);
 
         System.out.printf("Время работы - %f милисекунд\n", (float) (System.currentTimeMillis() - a));
 
         int[] n = {3819651, 1556668, 3217037, 841715, 1159890, 9781837, 9154101, 7549502, 8755695, 8677778};
         for (int i : n) {
-            System.out.print(arr[i] + "\t");
+            System.out.print(res[i] + "\t");
         }
         System.out.println();
     }
