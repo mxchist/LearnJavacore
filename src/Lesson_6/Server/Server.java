110ee15688f6ff0c188bff7295d4fd3293dd5cee
Maxim Chistyakov <gods.like.you@gmail.com>
2019-08-25 16:00:29 +0300

    Before the fx form

--- src/Lesson_6/Server/Server.java
+++ src/Lesson_6/Server/Server.java
@@ -1,6 +1,7 @@
-package Lesson_6;
+package Lesson_6.Server;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
@@ -17,14 +18,24 @@ public class Server {
             socket = server.accept();
             System.out.print("Клиент подключился!");
             Scanner sc = new Scanner(socket.getInputStream());
+            PrintWriter outpw = new PrintWriter(socket.getOutputStream(), true);
 
             while (true) {
                 String str = sc.nextLine();
                 System.out.println("Client " + str);
+                if (str.equals("/end")) break;
+                outpw.println("echo " + str);
             }
         }
         catch (IOException e) {
             e.printStackTrace();
+        }
+        finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
     }
