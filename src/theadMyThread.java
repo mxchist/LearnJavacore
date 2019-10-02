d32c47cb88a5c3df6f2ea87a546392b683114e6a
mchist <maxim.chistyakov@devim.team>
2019-10-02 19:39:54 +0300

    almost done

--- src/theadMyThread.java
+++ src/theadMyThread.java
@@ -2,6 +2,7 @@ import static java.lang.System.out;
 
 import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class theadMyThread {
@@ -16,13 +17,12 @@ public class theadMyThread {
 
     static float[] processArray(float[] array, int start) {
         for (int i = 0; i < array.length; i++) {
-            array[i + start] = (float)(Math.sin(0.2f + (i + start)/5) * Math.cos(0.2f + (i + start)/5) * Math.cos(0.4f + (i + start)/2));
+            array[i] = (float)(Math.sin(0.2f + (i + start)/5) * Math.cos(0.2f + (i + start)/5) * Math.cos(0.4f + (i + start)/2));
         }
         return array;
     }
 
-    static void multithreading () {
-        float[] myArray = new float[size];
+    static void multithreading (float[] myArray) {
         int threadCount, tempArraySize;
         int start = 0, end = start;
         int[] theNewArray = new int[size];
@@ -31,9 +31,16 @@ public class theadMyThread {
         try {
             out.printf("Введите желаемое количество потоков %n");
             threadCount = sc.nextInt();
+            tempArraySize = (int)(size / threadCount);
+            out.printf("%10.5f %n", myArray[100000]);
+            for (int i = 1; i < threadCount; i++) {
+                for (int j = -5; j < 5; j++) {
+                    out.printf("mayarray[%d]: %8.5f %5s", i*tempArraySize + j, myArray[i*tempArraySize + j], "");
+                }
+                out.println();
+            }
             fillArray(myArray);
 
-            tempArraySize = (int)(size / threadCount);
 //            ArrayList<float[]> tempArraySet = new ArrayList<float[]>();
             ArrayList<Thread> threadSet = new ArrayList<Thread>();
             ArrayList<float[]> resultArraySet = new ArrayList<float[]>();
@@ -41,7 +48,7 @@ public class theadMyThread {
 
             for (int currentThread = 0; currentThread < threadCount && start < myArray.length; currentThread++) {
                 end += tempArraySize;
-                final int startInRunnable = currentThread * tempArraySize;
+                final int startInRunnable = start;
                 if (end + tempArraySize >= size)  {
                     end = size - 1;
                     tempArraySize = end - start;
@@ -56,12 +63,18 @@ public class theadMyThread {
                     }
                 }));
                 start = end + 1;
+                threadSet.get(currentThread).start();
             }
 
-            out.printf("Количество тредов в коллекции: %d", threadSet.size());
+            out.printf("Количество тредов в коллекции: %d %n", threadSet.size());
 
             for (int currentThread = 0; currentThread < threadCount; currentThread++) {
-                    threadSet.get(currentThread).start();
+                try {
+                    threadSet.get(currentThread).join();
+                }
+                catch (InterruptedException exc) {
+                    exc.printStackTrace();
+                }
             }
 
             for (int currentThread = 0; currentThread < threadCount; currentThread++) {
@@ -69,6 +82,13 @@ public class theadMyThread {
                 , resultArraySet.get(currentThread).length);
             }
             out.printf("Затраченное время на выполнение: %.1f секунд %n", (float) (System.currentTimeMillis() / 1000 - startTime));
+            out.printf("%10.5f %n", myArray[100000]);
+            for (int i = 1; i < threadCount; i++) {
+                for (int j = -5; j < 5; j++) {
+                    out.printf("mayarray[%d]: %8.5f %5s", i*tempArraySize + j, myArray[i*tempArraySize + j], "");
+                }
+                out.println();
+            }
 
         } catch (InputMismatchException exc) {
             exc.printStackTrace();
@@ -80,13 +100,12 @@ public class theadMyThread {
         //всё в один поток
         float[] myArray = new float[size];
         fillArray(myArray);
-        out.println(myArray[100000]);
         long startTime = System.currentTimeMillis() / 1000;
         processArray(myArray, 0);
-        out.println(myArray[100000]);
+        out.printf("%10.5f %n", myArray[100000]);
         out.printf("Затраченное время на выполнение: %.1f секунд %n", (float) (System.currentTimeMillis() / 1000 - startTime));
 
         //Пробуем в многопоточность
-        multithreading();
+        multithreading(myArray);
     }
 }
