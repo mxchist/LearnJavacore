2c4654920770003c6f5ca716566b675693eac7ad
mchist <maxim.chistyakov@devim.team>
2019-10-22 20:14:36 +0300

    Exercises 1,2

--- src/generalization/Stats.java
+++ src/generalization/Stats.java
@@ -29,23 +29,44 @@ class Stats <T> {
 class AllwaysOutnumbered {
 
     static void changeThePlaceOfElements (Stats someArray, int pos1, int pos2) {
+        if (pos1 >= someArray.getObj().length || pos2 >= someArray.getObj().length || pos1 < 0 || pos2 < 0) {
+            out.println("Index of bound exception!");
+            return;
+        }
         Object temporaryObj = someArray.getObj()[pos1];
         someArray.getObj()[pos1] = someArray.getObj()[pos2];
         someArray.getObj()[pos2] = temporaryObj;
     }
 
-    ArrayList<Object> getArrayList (Stats stats) {
+    static boolean isThisArrayList(Object obj) {
+        if (obj instanceof ArrayList) return true;
+        return false;
+    }
+
+    static ArrayList<Object> getArrayList (Stats stats) {
         ArrayList<Object> al = new ArrayList<>();
-        for ()
+        for (Object t : stats.getObj()) {
+            al.add(t);
+        }
+        return al;
     }
 
     public static void main (String ... args) {
         //Задание 1
         Stats<String> stats1 = new Stats<>("Spitfire", "Memphis bells", "Girls", "Wake Up Call", "Action Radar");
-        changeThePlaceOfElements(stats1, 1, 23);
-        out.print( stats1.toString());
+        changeThePlaceOfElements(stats1, 1, 2);
+        out.println( stats1.toString());
 
         //Задание 2
-
+        out.println ("Задание 2");
+        out.println (isThisArrayList(stats1));
+        ArrayList<Object> al;
+        al = getArrayList(stats1);
+        out.println (isThisArrayList(al));
+        for (Object a : al) {
+            out.print(a.toString());
+            out.print("\t");
+        }
+        out.println();
     }
 }
