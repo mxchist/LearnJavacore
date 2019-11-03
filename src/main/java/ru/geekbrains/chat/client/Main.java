bb2b8c078bc5fbfba0eeda9e246d905bf92a158e
Max Chistyakov <ilovetolive@inbox.ru>
2019-11-03 15:46:36 +0300

    Try to resolve provlem

--- src/main/java/ru/geekbrains/chat/client/Main.java
+++ src/main/java/ru/geekbrains/chat/client/Main.java
@@ -1,18 +1,20 @@
 package ru.geekbrains.chat.client;
 
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
+
+import java.io.*;
+import java.nio.file.FileAlreadyExistsException;
+import java.util.Scanner;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("client.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("src\\main\\java\\ru\\geekbrains\\chat\\client\\client.fxml"));
         primaryStage.setTitle("2k18SummerChat");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
@@ -20,6 +22,22 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
+//        File file = new File("src\\main\\java\\ru\\geekbrains\\chat\\client");
+//        for (String f : file.list()) {
+//            System.out.println(f);
+//        }
+//        File f1 = new File("src\\main\\java\\ru\\geekbrains\\chat\\client\\client.fxml");
+//        Scanner sc;
+//        try {
+//            sc = new Scanner(f1);
+//            for (int i = 0; i < 5; i++) {
+//                System.out.println(sc.next());
+//            }
+//        }
+//        catch (FileNotFoundException exc) {
+//            System.out.println(exc.getMessage());
+//        }
+
         launch(args);
     }
 }
