c7785e024317b1f5b09d65fb69c3611b97de3b61
Max Chistyakov <ilovetolive@inbox.ru>
2020-04-17 10:12:06 +0300

    client/Main.java -> client/ClientMain.java

--- src/main/java/ru/geekbrains/chat/client/ClientMain.java
+++ src/main/java/ru/geekbrains/chat/client/ClientMain.java
@@ -10,19 +10,18 @@ import java.io.*;
 import java.nio.file.FileAlreadyExistsException;
 import java.util.Scanner;
 
-public class Main extends Application {
+public class ClientMain extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-//        Parent root = FXMLLoader.load(getClass().getResource("fxml/client.fxml"));
+//        Parent root = FXMLLoader.load(getClass().getResource("src\\main\\java\\ru\\geekbrains\\chat\\client\\client.fxml"));
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/client.fxml"));
 //        Parent root = FXMLLoader.load(getClass().getResource("client.fxml"));
 //        Parent root = FXMLLoader.load(getClass().getResource("src\\main\\java\\ru\\geekbrains\\chat\\client\\client.fxml"));
         primaryStage.setTitle("2k18SummerChat");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
-        // primaryStage.setOnCloseRequest(event -> System.out.println("On Close"));
     }
 
     public static void main(String[] args) {
