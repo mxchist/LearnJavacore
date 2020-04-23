839243e578ee39210400e13f012b8dfb27897c77
Max Chistyakov <ilovetolive@inbox.ru>
2020-04-24 00:56:31 +0300

    schema was changed

--- ddl_statement.sql
+++ ddl_statement.sql
@@ -1,11 +1,11 @@
+PRAGMA foreign_keys = on;
+
 CREATE TABLE user (
     user_id       INTEGER PRIMARY KEY AUTOINCREMENT,
     login    TEXT    UNIQUE,
     password INTEGER,
     nickname TEXT    UNIQUE
 );
-
-PRAGMA foreign_keys = on;
 
 CREATE TABLE student (student_id integer primary key autoincrement
     , student_name varchar(100)
@@ -18,19 +18,18 @@ create table server_session (
 );
 
 CREATE TABLE user_session (
-    user_session_id   INTEGER,
-    server_session_id INTEGER REFERENCES server_session (server_session_id),
+    user_session_id   INTEGER PRIMARY KEY AUTOINCREMENT,
+    server_session_id INTEGER NOT NULL  references server_session (server_session_id),
     creation_time     TEXT    NOT NULL
-                              CONSTRAINT DF_creation_time DEFAULT (datetime('now', 'localtime') ),
-    nickname          TEXT,
-    constraint PK_user_session PRIMARY KEY (user_session_id, server_session_id)
-);
+		CONSTRAINT DF_creation_time DEFAULT (datetime('now', 'localtime') ),
+    nickname          TEXT);
 
 create table messages_broadcast (
     id integer primary key autoincrement
     , creation_time     TEXT    NOT NULL
                               CONSTRAINT DF_creation_time DEFAULT (datetime('now', 'localtime') )
-    , server_session_id integer
+    , server_session_id integer references server_session (server_session_id)
+    , user_session_id integer        references user_session (user_session_id)
     , message text
 );
 
@@ -38,7 +37,8 @@ create table messages_personal (
     id integer primary key autoincrement
     , creation_time     TEXT    NOT NULL
                               CONSTRAINT DF_creation_time DEFAULT (datetime('now', 'localtime') )
-    , sent_user_session_id integer
-    , recieved_user_session_id integer
+    , sent_user_session_id integer        references user_session (user_session_id)
+    , recieved_user_session_id integer    references user_session (user_session_id)
     , message text
-)
+);
+
