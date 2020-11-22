71cc5f76cae34c5be155d42f6b7db6583a3b3b92
Max Chistyakov <ilovetolive@inbox.ru>
2020-11-22 18:08:38 +0300

    The button for upload was added to the personal controller

--- src/main/java/ru/geekbrains/chat/client/PersonalController.java
+++ src/main/java/ru/geekbrains/chat/client/PersonalController.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
+import java.io.File;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -12,22 +14,32 @@ import java.util.ResourceBundle;
 
 public class PersonalController {
     @FXML
-    Button btn;
+    Button btnSent;
+
+    @FXML
+    Button btnUpload;
 
     @FXML
     TextArea textArea;
 
     public void btnClick() {
-        if(!((MiniStage)btn.getScene().getWindow()).parentList.contains(textArea)) {
-            ((MiniStage)btn.getScene().getWindow()).parentList.add(textArea);
+        if(!((MiniStage)btnSent.getScene().getWindow()).parentList.contains(textArea)) {
+            ((MiniStage)btnSent.getScene().getWindow()).parentList.add(textArea);
         }
-        DataOutputStream out = ((MiniStage)btn.getScene().getWindow()).out;
-        String nickTo = ((MiniStage)btn.getScene().getWindow()).nickTo;
+        DataOutputStream out = ((MiniStage)btnSent.getScene().getWindow()).out;
+        String nickTo = ((MiniStage)btnSent.getScene().getWindow()).nickTo;
         try {
             out.writeUTF("/w " + nickTo + " " + textArea.getText());
             textArea.clear();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void buttonClickUpload() {
+        DataOutputStream out = ((MiniStage)btnUpload.getScene().getWindow()).out;
+
+        FileChooser fileChooser = new FileChooser();
+        File fileToUpload = fileChooser.showOpenDialog((MiniStage)btnUpload.getScene().getWindow());
     }
 }
