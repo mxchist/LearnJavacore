742f19b748c4c69cff841fef17c28bf72d6c50b6
Max Chistyakov <ilovetolive@inbox.ru>
2021-01-06 00:40:33 +0300

    Print ABC: второй способ реализации

--- src/main/java/exercise4/Main.java
+++ src/main/java/exercise4/Main.java
@@ -5,8 +5,34 @@ public class Main {
 		//Создать три потока, каждый из которых выводит определенную букву
 		// (A, B и C) 5 раз (порядок – ABСABСABС). Используйте wait/notify/notifyAll.
 		PrintChar pc = new PrintChar();
-		pc.v();
+//		Первый способ реализации
+//		pc.v();
 
+//		Второй способ реализации
+		Thread t1 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				pc.printA();
+			}
+		});
+
+		Thread t2 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				pc.printB();
+			}
+		});
+
+		Thread t3 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				pc.printC();
+			}
+		});
+
+		t1.start();
+		t2.start();
+		t3.start();
 
 	}
 }
